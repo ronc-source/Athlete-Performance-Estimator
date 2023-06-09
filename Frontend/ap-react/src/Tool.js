d40b5c4b0bee970a9f91b169235ef6c9bee1cdeb
Ronc-Source <ronniecharlesbusiness@outlook.com>
2023-06-09 14:13:19 -0400

    Added search bar functionality with dropdown suggestions to Tool page. Player JSON list and Search Bar component are added.

--- Frontend/ap-react/src/Tool.js
+++ Frontend/ap-react/src/Tool.js
@@ -1,7 +1,10 @@
+import SearchBar from './components/SearchBar'
+
 function Tool() {
     return(
         <div className="home">
             <h2>Tool Page</h2>
+            <SearchBar />
         </div>
     );
 }
